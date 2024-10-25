e64b07e97be48a331877060a216696107c9fa97b
Hacker_Hub <140789640+SpiderHacker450@users.noreply.github.com>
2024-10-25 08:09:55 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt install figlet  pv xterm
+sudo apt install figlet  pv xterm python3 python-is-python3
 sudo cp main.sh /main.sh
 sudo cp convertor.py /
 sudo ln -sf /main.sh /usr/local/bin/tercus
